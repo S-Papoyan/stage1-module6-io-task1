0de3ab525fddf376ef06a435c56ff768b237a62e
Sergey <spapoyan1987@gmail.com>
2022-12-20 22:23:04 +0400

    commit

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,28 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public static Profile getDataFromFile(File file) {
+        StringBuilder temp = new StringBuilder();
+        try (FileInputStream in = new FileInputStream(file)) {
+            int ch;
+            while ((ch = in.read()) != -1) {
+                char a = (char) ch;
+                temp.append(a);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] split = temp.toString().split("\r\n");
+        String[] values = new String[4];
+        for (int i = 0; i < split.length; i++) {
+            String[] split1 = split[i].split(": ");
+            values[i] = split1[1];
+        }
+        return new Profile(values[0], Integer.parseInt(values[1]), values[2], Long.parseLong(values[3]));
     }
 }
